0f51fc67894bf5fddf1871cc49f8a92a4f077620
daredavil01 <sanket.tambare01@gmail.com>
2021-11-29 00:45:33 +0530

    Updated alembic

--- app/models.py
+++ app/models.py
@@ -29,7 +29,6 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
-    phone_number = Column(String, nullable=False)
 
 
 class Vote(Base):
